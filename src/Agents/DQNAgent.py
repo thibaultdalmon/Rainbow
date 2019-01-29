cc26bb609799d5f7fb94ae513d9df893a2394a97
Thibault Dalmon <thibault.dalmon@telecom-paristech.fr>
2019-01-29 15:24:01 +0100

    Keras pas assez souple, passage en tensorflow->débogué

--- src/Agents/DQNAgent.py
+++ src/Agents/DQNAgent.py
@@ -1,75 +1,112 @@
 import numpy as np
-
-from keras.layers import Input, Conv2D, Flatten, Dense
-from keras.models import Model
-from keras.optimizers import Adam
-import keras.backend as K
+import tensorflow as tf
 
 class DQNAgent:
 
     def __init__(self, env):
+
+        # to make this output stable across runs
+        def reset_graph(seed=42):
+            tf.reset_default_graph()
+            tf.set_random_seed(seed)
+            np.random.seed(seed)
+        reset_graph()
+
         self.env = env
-        self.action_size = self.env.action_space.n
-        self.input_height = 105
+        self.input_height = 96
         self.input_width = 80
-        self.discount_rate = 0.95    # discount rate
-        self.epsilon = 0.01  # exploration rate
+
+        self.input_channels = 1
+        self.conv_n_maps = [32, 64, 64]
+        self.conv_kernel_sizes = [(8,8), (4,4), (3,3)]
+        self.conv_strides = [4, 2, 1]
+        self.conv_paddings = ["SAME"] * 3
+        self.conv_activation = [tf.nn.relu] * 3
+        self.n_hidden_in = 64 * 12 * 10
+        self.n_hidden = 512
+        self.hidden_activation = tf.nn.relu
+        self.n_outputs = env.action_space.n
+        self.initializer = tf.variance_scaling_initializer()
+
         self.learning_rate = 0.001
-        self.model = self.qNetwork()
         self.memory = ReplayMemory()
-        self.max_q_value = 0
-        self.loss_val = 0
 
-    def choose_action(self, observation):
-        if np.random.rand() <= self.epsilon:
-            return self.env.action_space.sample()
-        act_values = self.model.predict(observation)
-        self.max_q_value = np.max(act_values)
-        return np.argmax(act_values[0])
+        self.eps_min = 0.1
+        self.eps_max = 1.0
+        self.eps_decay_steps = 200000
 
-    def preprocessing(self, observation):
-        img = observation[::2,::2] # downsize
+        def q_network(X_state, name):
+            prev_layer = X_state / 128.0
+            with tf.variable_scope(name) as scope:
+                for n_maps, kernel_size, strides, padding, activation in zip(
+                        self.conv_n_maps, self.conv_kernel_sizes, self.conv_strides,
+                        self.conv_paddings, self.conv_activation):
+                    prev_layer = tf.layers.conv2d(
+                        prev_layer, filters=n_maps, kernel_size=kernel_size,
+                        strides=strides, padding=padding, activation=activation,
+                        kernel_initializer=self.initializer)
+                last_conv_layer_flat = tf.reshape(prev_layer, shape=[-1, self.n_hidden_in])
+                hidden = tf.layers.dense(last_conv_layer_flat, self.n_hidden,
+                                         activation=self.hidden_activation,
+                                         kernel_initializer=self.initializer)
+                outputs = tf.layers.dense(hidden, self.n_outputs,
+                                          kernel_initializer=self.initializer)
+            trainable_vars = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES,
+                                               scope=scope.name)
+            trainable_vars_by_name = {var.name[len(scope.name):]: var
+                                      for var in trainable_vars}
+            return outputs, trainable_vars_by_name
+
+        self.X_state = tf.placeholder(tf.float32, shape=[None, self.input_height, self.input_width,
+                                            self.input_channels])
+        self.online_q_values, self.online_vars = q_network(self.X_state, name="q_networks/online")
+        self.target_q_values, self.target_vars = q_network(self.X_state, name="q_networks/target")
+
+        self.copy_ops = [target_var.assign(self.online_vars[var_name])
+                    for var_name, target_var in self.target_vars.items()]
+        self.copy_online_to_target = tf.group(*self.copy_ops)
+
+        with tf.variable_scope("train"):
+            self.X_action = tf.placeholder(tf.int32, shape=[None])
+            self.y = tf.placeholder(tf.float32, shape=[None, 1])
+            q_value = tf.reduce_sum(self.online_q_values * tf.one_hot(self.X_action, self.n_outputs),
+                                    axis=1, keepdims=True)
+            error = tf.abs(self.y - q_value)
+            clipped_error = tf.clip_by_value(error, 0.0, 1.0)
+            linear_error = 2 * (error - clipped_error)
+            self.loss = tf.reduce_mean(tf.square(clipped_error) + linear_error)
+
+            self.global_step = tf.Variable(0, trainable=False, name='global_step')
+            optimizer = tf.train.AdamOptimizer(self.learning_rate)
+            self.training_op = optimizer.minimize(self.loss, global_step=self.global_step)
+
+        self.init = tf.global_variables_initializer()
+        self.saver = tf.train.Saver()
+
+
+    def preprocess_observation(self, observation):
+        img = observation[5:197:2,::2] # downsize
         img = img.sum(axis=2) # to greyscale
-        img = (img // 3 - 128).astype(np.int8) # normalize from -128 to 127
-        return img.reshape(1, 105, 80, 1)
+        img = (img / (3*128) - 128)
+        return img.reshape(96, 80, 1)
 
     def remember(self, data):
         self.memory.append(data)
 
-    def qNetwork(self):
-        input = Input(shape=(self.input_height, self.input_width,1))
-        conv1 = Conv2D(filters=32, kernel_size=(8,8), activation='relu')(input)
-        conv2 = Conv2D(filters=32, kernel_size=(4,4), activation='relu')(conv1)
-        conv3 = Conv2D(filters=64, kernel_size=(3,3), activation='relu')(conv2)
-        flat = Flatten()(conv3)
-        dense1 = Dense(512, activation='relu')(flat)
-        dense2 = Dense(self.action_size)(dense1)
-        model = Model(inputs=input, outputs=dense2)
+    def epsilon_greedy(self, q_values, step):
+        epsilon = max(self.eps_min, self.eps_max - (self.eps_max-self.eps_min) * step/self.eps_decay_steps)
+        if np.random.rand() < epsilon:
+            return np.random.randint(self.n_outputs) # random action
+        else:
+            return np.argmax(q_values) # optimal action
 
-        def _loss_dqn(y_true, y_pred):
-            q_value = K.sum(y_pred * K.one_hot(K.argmax(y_pred),
-                                    self.action_size),
-                                    axis=1, keepdims=True)
-            error = K.abs(y_true - q_value)
-            clipped_error = K.clip(error, 0.0, 1.0)
-            linear_error = 2 * (error - clipped_error)
-            loss = K.mean(K.square(clipped_error) + linear_error)
-            self.loss_val = loss
-            return loss
-
-        model.compile(loss = _loss_dqn,
-            optimizer = Adam(lr=self.learning_rate))
-        return model
-
-    def train(self, batch_size):
-        states, actions, rewards, next_states, done = (
-            self.memory.sample_memories(batch_size))
-        targets = rewards
-        q_values = self.model.predict(next_states)
-        max_q_values = np.max(q_values, axis=1, keepdims=True)
-        y_values = rewards + done * self.discount_rate * max_q_values
-
-        self.model.fit(states, y_values, epochs=1)
+    def sample_memories(self, batch_size):
+        cols = [[], [], [], [], []] # state, action, reward, next_state, done
+        for memory in self.memory.sample(batch_size):
+            for col, value in zip(cols, memory):
+                col.append(value)
+        cols = [np.array(col) for col in cols]
+        return cols[0], cols[1], cols[2].reshape(-1, 1), cols[3], cols[4].reshape(-1, 1)
 
 
 class ReplayMemory:
@@ -90,12 +127,3 @@ class ReplayMemory:
         else:
             indices = np.random.permutation(self.length)[:batch_size]
         return self.buf[indices]
-
-    def sample_memories(self, batch_size):
-        cols = [[], [], [], [], []] # state, action, reward, next_state, done
-        for memory in self.sample(batch_size):
-            for col, value in zip(cols, memory):
-                col.append(value)
-        cols = [np.array(col) for col in cols]
-        return (cols[0].reshape(batch_size, 105, 80, 1), cols[1], cols[2],
-                cols[3].reshape(batch_size, 105, 80, 1), cols[4])
--- src/ReinforcementLearning/Trainer.py
+++ src/ReinforcementLearning/Trainer.py
@@ -3,7 +3,9 @@ from Agents.DQNAgent import DQNAgent
 from utils.Plots import plot_rewards
 
 import gym
+import os
 import numpy as np
+import tensorflow as tf
 
 class Trainer:
 
@@ -11,12 +13,21 @@ class Trainer:
         self.env = gym.make(args.env_name)
         self.agent = RandomAgent(self.env)
         self.agent_dqn = DQNAgent(self.env)
-        self.n_episode = args.n_episodes
+        self.n_episode = args.n_episodes # nb of games to play
+        self.training_start = 10000  # start training after 10,000 game iterations
+        self.training_interval = 4  # run a training step every 4 game iterations
+        self.save_steps = 1000  # save the model every 1,000 training steps
+        self.copy_steps = 10000  # copy online DQN to target DQN every 10,000 training steps
+        self.discount_rate = 0.99
+        self.skip_start = 0  # Skip the start of every game
+        self.batch_size = 50
+        self.iteration = 0  # game iterations
         self.step = 0
         self.loss_val = np.infty
         self.game_length = 0
         self.total_max_q = 0
         self.mean_max_q = 0.0
+        self.checkpoint_path = "../DQN/DQN_test.ckpt"
 
         self.list_of_rewards = []
 
@@ -36,33 +47,68 @@ class Trainer:
         self.env.close()
 
     def run_dqn(self):
-        observation = self.env.reset()
-        observation = self.agent_dqn.preprocessing(observation)
         done = True
-        for step in range(self.n_episode):
-            print("Training step {}/{} ({:.1f})%\tLoss {:5f}\tMean Max-Q {:5f}   \n".format(
-            step, self.n_episode, step * 100 / self.n_episode, self.loss_val, self.mean_max_q), end="")
-            if done:
-                observation = self.env.reset()
-                observation = self.agent_dqn.preprocessing(observation)
+        with tf.Session() as sess:
+            if os.path.isfile(self.checkpoint_path + ".index"):
+                self.agent_dqn.saver.restore(sess, self.checkpoint_path)
+            else:
+                self.agent_dqn.init.run()
+                self.agent_dqn.copy_online_to_target.run()
+            while True:
+                step = self.agent_dqn.global_step.eval()
+                if step >= self.n_episode:
+                    break
+                self.iteration += 1
+                print("\rIteration {}\tTraining step {}/{} ({:.1f})%\tLoss {:5f}\tMean Max-Q {:5f}   ".format(
+                    self.iteration, step, self.n_episode, step * 100 / self.n_episode,
+                    self.loss_val, self.mean_max_q), end="")
+                if done: # game over, start again
+                    obs = self.env.reset()
+                    for skip in range(self.skip_start): # skip the start of each game
+                        obs, reward, done, info = self.env.step(0)
+                    state = self.agent_dqn.preprocess_observation(obs)
 
-            action = self.agent_dqn.choose_action(observation)
-            next_state, reward, done, info = self.env.step(action)
+                # Online DQN evaluates what to do
+                q_values = self.agent_dqn.online_q_values.eval(feed_dict={self.agent_dqn.X_state: [state]})
+                action = self.agent_dqn.epsilon_greedy(q_values, step)
 
-            next_state = self.agent_dqn.preprocessing(next_state)
-            self.agent_dqn.remember((observation, action, reward,
-                next_state, done))
+                # Online DQN plays
+                obs, reward, done, info = self.env.step(action)
+                next_state = self.agent_dqn.preprocess_observation(obs)
 
-            observation = next_state
-            self.total_max_q += self.agent_dqn.max_q_value
-            self.game_length += 1
-            if done:
-                self.mean_max_q = self.total_max_q / self.game_length
-                self.total_max_q = 0.0
-                self.game_length = 0
+                # Let's memorize what happened
+                self.agent_dqn.memory.append((state, action, reward, next_state, 1.0 - done))
+                state = next_state
 
-            if (self.step%10 == 9):
-                self.agent_dqn.train(50)
-                self.loss_val = self.agent_dqn.loss_val
+                # Compute statistics for tracking progress
+                self.total_max_q += q_values.max()
+                self.game_length += 1
+                if done:
+                    self.mean_max_q = self.total_max_q / self.game_length
+                    self.total_max_q = 0.0
+                    self.game_length = 0
 
-            self.step+=1
+                if self.iteration < self.training_start or self.iteration % self.training_interval != 0:
+                    continue # only train after warmup period and at regular intervals
+
+                # Sample memories and use the target DQN to produce the target Q-Value
+                X_state_val, X_action_val, rewards, X_next_state_val, continues = (
+                    self.agent_dqn.sample_memories(self.batch_size))
+                next_q_values = self.agent_dqn.target_q_values.eval(
+                    feed_dict={self.agent_dqn.X_state: X_next_state_val})
+                max_next_q_values = np.max(next_q_values, axis=1, keepdims=True)
+                y_val = rewards + continues * self.discount_rate * max_next_q_values
+
+                # Train the online DQN
+                _, self.loss_val = sess.run([self.agent_dqn.training_op, self.agent_dqn.loss],
+                    feed_dict={self.agent_dqn.X_state: X_state_val,
+                    self.agent_dqn.X_action: X_action_val,
+                    self.agent_dqn.y: y_val})
+
+                # Regularly copy the online DQN to the target DQN
+                if step % self.copy_steps == 0:
+                    self.agent_dqn.copy_online_to_target.run()
+
+                # And save regularly
+                if step % self.save_steps == 0:
+                    self.agent_dqn.saver.save(sess, self.checkpoint_path)
